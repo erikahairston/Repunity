c4d29b0fb583403eb5dde3a4c0716fb5a081caa9
Erika Hairston <erikahairston@gmail.com>
2018-05-05 20:32:41 -0400

    adding constraints pt.2

--- Repunity/profileViewController.swift
+++ Repunity/profileViewController.swift
@@ -22,30 +22,37 @@ class profileViewController: UIViewController {
     @IBOutlet weak var jobAtCompanyLabel: UILabel!
     @IBOutlet weak var majorAtSchoolLabel: UILabel!
     @IBOutlet weak var supportGroups: UILabel!
+    @IBOutlet weak var coverPhotoImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         profileImageView.layer.cornerRadius = profileImageView.bounds.height / 2
         profileImageView.clipsToBounds = true
-        
-        if currRoleModel != nil {
-            nameLabel.text = currRoleModel.name
-            ImageService.getImage(withURL: currRoleModel.imgURL) { (image) in
-                self.profileImageView.image = image
+        coverPhotoImage.image = rotateCoverImage()
+    
+        nameLabel.text = currRoleModel.name
+        ImageService.getImage(withURL: currRoleModel.imgURL) { (image) in
+            self.profileImageView.image = image
             }
-            funFactLabel.text = "\"" + currRoleModel.funFact + "\""
-            industryLabel.text = "In the " + currRoleModel.industry + " Industry"
-            supportGroups.text = currRoleModel.relevantGroups
-            jobAtCompanyLabel.text = currRoleModel.currOccupation + " at " + currRoleModel.currEmployer
-            majorAtSchoolLabel.text =  currRoleModel.primaryMajor + " major at " + currRoleModel.undergradCollege
-        }
-
-
-        // Do any additional setup after loading the view.
+        funFactLabel.text = "\"" + currRoleModel.funFact + "\""
+        industryLabel.text = "In the " + currRoleModel.industry + " Industry"
+        supportGroups.text = "Supportive Groups: " + currRoleModel.relevantGroups
+        jobAtCompanyLabel.text = "Experience: " + currRoleModel.currOccupation + " at " + currRoleModel.currEmployer
+        majorAtSchoolLabel.text = "Education: " + currRoleModel.primaryMajor + " major at " + currRoleModel.undergradCollege
     }
     
     //actions
     @IBAction func msgButtonClicked(_ sender: Any) {
+    }
+    
+    //functions
+    func rotateCoverImage() -> UIImage {
+        var chosenPhoto = UIImage()
+        var possPhotos = ["blue_cover.jpg", "cloud_cover.jpg", "kelli-tungay-324329-unsplash.jpg", "nathan-anderson-109933-unsplash.jpg", "yellow_cover.jpg"]
+        let numPhotos = possPhotos.count - 1
+        let randIndx = Int(arc4random_uniform(UInt32(numPhotos)))
+        chosenPhoto = UIImage(named: possPhotos[randIndx])!
+        return chosenPhoto
     }
    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -54,6 +61,4 @@ class profileViewController: UIViewController {
             destinationVC.selectedUserToChat = currRoleModel
         }
     }
-    
-
 }
